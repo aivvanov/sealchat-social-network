4283bf0919d90a91c2bb720011c203f9bfe9f515
Xander Ivanov <sanja_ak@mail.ru>
2024-09-30 23:55:20 +0300

    Index.js and state.js refactoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from "./redux/state";
-import { rerenderEntireTree } from './render';
+import state, { subscribe, sendMessage, addPost, updateMessageText, updatePostText } from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export let rerenderEntireTree = () => {
+  root.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <App state={state} addPost={addPost} updatePostText={updatePostText} sendMessage={sendMessage} updateMessageText={updateMessageText} />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
