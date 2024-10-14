ebe23b350eb20bf374c17d229c97095677f2655b
Xander Ivanov <sanja_ak@mail.ru>
2024-10-15 00:07:56 +0300

    Get users list using axios query

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,71 +1,16 @@
 import React from "react";
 import styles from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/userPhoto.jpeg';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: "1",
-                fullName: "Xander Ivanov",
-                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSId_pIi48v4SnNz81LkwY8hqViBBw73_hy_w&s",
-                location: {
-                    id: 1,
-                    city: "Moscow",
-                    country: "Russia"
-                },
-                isFollowed: true,
-                statusText: "Hello! How you guys doing here?"
-            },
-            {
-                id: "2",
-                fullName: "Tara Basro",
-                photoUrl: "https://cdn1-production-images-kly.akamaized.net/xO8KZWeoffVVctm4WuQL6eoFt20=/800x1066/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/1948948/original/042673800_1519880576-FOTO_1.jpg",
-                location: {
-                    id: 1,
-                    city: "New-York",
-                    country: "USA"
-                },
-                isFollowed: false,
-                statusText: "I`m here!!!!"
-            },
-            {
-                id: "3",
-                fullName: "Daniel Adnan",
-                photoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWvongFeVghuPM8EkKOenRpSDxn8ffL48Xkg&s",
-                location: {
-                    id: 1,
-                    city: "London",
-                    country: "England UK"
-                },
-                isFollowed: true,
-                statusText: "hey hey hey!"
-            },
-            {
-                id: "4",
-                fullName: "Mia Goth",
-                photoUrl: "https://ntvb.tmsimg.com/assets/assets/754303_v9_bc.jpg",
-                location: {
-                    id: 1,
-                    city: "Moscow",
-                    country: "Russia"
-                },
-                isFollowed: false,
-                statusText: "Idk what to do"
-            },
-            {
-                id: "5",
-                fullName: "Lily Collins",
-                photoUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/552822_v9_bc.jpg",
-                location: {
-                    id: 1,
-                    city: "Chicago",
-                    country: "USA"
-                },
-                isFollowed: false,
-                statusText: "Just wanna talk with someone :("
-            }
-        ]);
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            })
     }
 
     return (
@@ -75,17 +20,17 @@ const Users = (props) => {
                     return (
                         <div key={user.id} className={styles.user_card}>
                             <div className={styles.user_info}>
-                                <img className={styles.user_photo} src={user.photoUrl} alt="userPhoto" />
+                                <img className={styles.user_photo} src={user.photos.small ? user.photos.small : userPhoto} alt="userPhoto" />
                                 <div className={styles.user_details}>
-                                    <div className={styles.user_name}>{user.fullName}</div>
-                                    <div className={styles.user_status}>{user.statusText}</div>
+                                    <div className={styles.user_name}>{user.name}</div>
+                                    <div className={styles.user_status}>{user.status ? user.status : "Unknown status"}</div>
                                     <div className={styles.user_location}>
-                                        {user.location.city}, {user.location.country}
+                                        {user.city ? user.city : "Unknown city"}, {user.country ? user.country : "Unknown country"}
                                     </div>
                                 </div>
                             </div>
                             <div className={styles.action_button}>
-                                {user.isFollowed
+                                {user.followed
                                     ? <button className={styles.unfollow_button} onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
                                     : <button className={styles.follow_button} onClick={() => { props.follow(user.id) }}>Follow</button>
                                 }
