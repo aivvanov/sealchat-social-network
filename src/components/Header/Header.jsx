aab2b650d885d93c7af5187cd992d68aff68fb56
Xander Ivanov <sanja_ak@mail.ru>
2024-10-29 00:39:12 +0300

    Edit Header component, add HeaderContainer and auth-reducer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,19 @@ import styles from './Header.module.css';
 import { NavLink } from "react-router-dom";
 import headerIcon from "../../assets/images/seals_header.svg"
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={styles.header}>
       <NavLink to="/profile" className={styles.icon_container}>
         <img className={styles.icon} src={headerIcon} alt="headerIcon" />
         <div className={styles.icon_text}>sealChat</div>
       </NavLink>
+      <div className={styles.loginBlock}>
+        {props.isAuth
+          ? <div>{props.login}</div>
+          : <NavLink to="/login">Log in</NavLink>
+        }
+      </div>
     </header>
   );
 }
