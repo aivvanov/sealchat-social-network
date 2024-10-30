ede6ce0932bfc6ad945c0585c2175fb971cec1a9
Xander Ivanov <sanja_ak@mail.ru>
2024-10-30 17:46:26 +0300

    Add data access layer to api.js

---
+++ src/api/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+    headers: {
+        "API-KEY": process.env.REACT_APP_API_KEY
+    }
+
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize) {
+        return instance
+            .get(`users?page=${currentPage ? currentPage : 1}&count=${pageSize}`, {
+                withCredentials: true
+            })
+            .then(response => {
+                return response.data;
+            })
+    },
+    searchUsersRequest(userSearchText, pageSize) {
+        return instance
+            .get(`users?term=${userSearchText}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            })
+    },
+    follow(userId) {
+        return instance
+            .post(`follow/${userId}`)
+            .then(response => {
+                return response.data;
+            })
+    },
+    unfollow(userId) {
+        return instance
+            .delete(`follow/${userId}`)
+            .then(response => {
+                return response.data;
+            })
+
+    }
+}
