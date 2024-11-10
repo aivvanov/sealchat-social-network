6a48d9bd32354607d2c2472908e1c2be515de5b7
Xander Ivanov <sanja_ak@mail.ru>
2024-11-10 22:00:29 +0300

    Minor changes

--- src/api/api.js
+++ src/api/api.js
@@ -44,7 +44,6 @@ export const usersAPI = {
             .then(response => {
                 return response.data;
             })
-
     }
 };
 
--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -13,11 +13,11 @@ export const withAuthRedirect = (Component) => {
     class RedirectComponent extends React.Component {
         render() {
             if (!this.props.isAuth) return <Navigate to='/login' />
-
+            
             return <Component { ...this.props} />
         }
     }
-    
+
     const ConnectedRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent);
 
     return ConnectedRedirectComponent;
