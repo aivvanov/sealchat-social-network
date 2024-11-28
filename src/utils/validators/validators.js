a744db7c5a152e97983dd56e515cccf311ecd6d2
Xander Ivanov <sanja_ak@mail.ru>
2024-11-29 00:43:58 +0300

    Add validators to Posts component's form

---
+++ src/utils/validators/validators.js
@@ -0,0 +1,11 @@
+export const required = value => {
+    if (!value) return "Field is required";
+
+    return undefined;
+}
+
+export const maxLengthCreator = (maxLength) => (value) => {
+    if (value && value.length > maxLength) return "Max length is 30 symbols";
+
+    return undefined;
+}
