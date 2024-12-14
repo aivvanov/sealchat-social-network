4a052487590e0f8cdeb76500d53d6217d8c95296
Xander Ivanov <sanja_ak@mail.ru>
2024-12-14 22:31:19 +0300

    Edit App component to initialize app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Info from './components/Info/Info';
@@ -11,29 +11,41 @@ import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
+import { connect } from 'react-redux';
+import { auth } from './redux/auth-reducer';
+import { withRouter } from './withRouter';
+import { compose } from 'redux';
 // import { Footer } from './components/Footer/Footer';
 
 
-const App = (props) => {
-  return (
-    <div className='app-wrapper'>
-      <HeaderContainer />
-      <NavbarContainer />
-      <div className='app-wrapper-content'>
-        <Routes>
-          <Route path='/profile/:userId?' element={<ProfileContainer />} />
-          <Route path='/dialogs/*' element={<DialogsContainer />} />
-          <Route path='/info' element={<Info />} />
-          <Route path='/news' element={<News />} />
-          <Route path='/music' element={<Music />} />
-          <Route path='/users' element={<UsersContainer />} />
-          <Route path='/settings' element={<Settings />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+class App extends Component {
+  componentDidMount() {
+    this.props.auth();
+  }
+
+  render() {
+    return (
+      <div className='app-wrapper'>
+        <HeaderContainer />
+        <NavbarContainer />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile/:userId?' element={<ProfileContainer />} />
+            <Route path='/dialogs/*' element={<DialogsContainer />} />
+            <Route path='/info' element={<Info />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/users' element={<UsersContainer />} />
+            <Route path='/settings' element={<Settings />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
       </div>
-      {/* <Footer /> */}
-    </div>
-  );
+    )
+  };
 }
 
-export default App;
+export default compose(
+  withRouter,
+  connect(null, {auth}))(App);
