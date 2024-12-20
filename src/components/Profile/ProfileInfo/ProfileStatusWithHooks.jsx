ac9ce58ca45637b5b21d941a8be629d0e2ecca61
Xander Ivanov <sanja_ak@mail.ru>
2024-12-21 01:11:51 +0300

    Refactor ProfileStatus component using hooks

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import styles from "./ProfileInfo.module.css";
+
+const ProfileStatusWithHooks = (props) => {
+
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            <div>
+                {!editMode &&
+                    <div
+                        onDoubleClick={activateEditMode}
+                        className={styles.user_description}>
+                        {props.status || <div className={styles.no_status_text}>Set status... ✍️</div>}
+                    </div>
+                }
+                {editMode &&
+                    <div className={styles.status_text}>
+                        <input
+                            type="text"
+                            className={styles.status_input}
+                            value={status}
+                            autoFocus
+                            onBlur={deactivateEditMode}
+                            onChange={onStatusChange}
+                        />
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks;
